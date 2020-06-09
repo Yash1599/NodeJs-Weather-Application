83982f5409f668d8537be5e5f632eaf2f5840020
Yash1599 <41269915+Yash1599@users.noreply.github.com>
2020-06-09 17:05:44 +0530

    Small Modifications

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client Side JavaScript file loaded!')
+//console.log('Client Side JavaScript file loaded!')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
